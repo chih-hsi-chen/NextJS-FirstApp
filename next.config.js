5f152b79fb44b10022734ba009ba0641469cd437
chih-hsi-chen <chih.hsi.chen@gmail.com>
2020-04-20 21:08:30 +0800

    add custom server

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
-// const {PHASE_DEVELOPMENT_SERVER} = require('next/constants');
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+const dotenv = require('dotenv');
+const path = require('path');
 
-module.exports = {
-    devIndicators: {
-        autoPrerender: true,
-    },
-}
+let result = null;
+
+module.exports = (phase, { defaultConfig }) => {
+    if(phase === PHASE_DEVELOPMENT_SERVER) {
+        result = dotenv.config();
+    } else {
+        result = dotenv.config({
+            path: path.resolve(process.cwd(), '.env.production'),
+        });
+    }
+    return {
+        env: {
+            NODE_ENV: process.env
+        },
+    }
+};
